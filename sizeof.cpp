02ac87b8a9d3842261b25b58b87352ef6374b9f8
ankur-kesari <ankurkesari05@gmail.com>
2020-10-22 23:57:03 +0530

    size of datatypes

---
+++ sizeof.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int a;
+    cout<<"size of int:"<<sizeof(a)<<endl;
+    float b;
+    cout<<"size of float:"<<sizeof(b)<<endl;
+    double c;
+    cout<<"size of double:"<<sizeof(c)<<endl;
+    char f;
+    cout<<"size of char:"<<sizeof(f)<<endl;
+    short int g;
+    cout<<"size of short int:"<<sizeof(g)<<endl;
+    bool bll;
+    cout<<"size of boolean:"<<sizeof(bool)<<endl;
+    return 0;
+}
