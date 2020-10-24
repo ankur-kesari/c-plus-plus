27c7c974eb775beb95591390264837446f56899d
ankur-kesari <ankurkesari05@gmail.com>
2020-10-24 22:00:26 +0530

    even and odd number

---
+++ even_odd.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"enter number:";
+    cin>>a;
+    if(a%2==0)
+    {
+        cout<<a<<" is even number"<<endl;
+    }
+    else
+    {
+        cout<<a<<" is odd number"<<endl;
+    }
+    
+    return 0;
+}
