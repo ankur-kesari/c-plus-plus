31b0a4d028ce343db4b85976f49ab1509deb7bff
ankur-kesari <ankurkesari05@gmail.com>
2020-10-27 19:11:27 +0530

    prime number

---
+++ prime_number.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num,i;
+    cout<<"enter the number :";
+    cin>>num;
+    for ( i = 2; i < num; i++)
+    {
+        if (num%i==0)
+        {
+            cout<<"not a prime number";
+            break;
+        }
+    }   
+        if (i==num)
+        {
+            cout<<"prime number";
+        } 
+        return 0;
+    
+    
+}
