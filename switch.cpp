47998eec44c30b2ef8b90b833de03028caf8c08b
ankur-kesari <ankurkesari05@gmail.com>
2020-10-27 20:39:07 +0530

    switch case

---
+++ switch.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int d;
+    cout << "enter number for day :";
+    cin>>d;
+    switch (d)
+    {
+    case 1:
+    cout<<"Sunday";
+        break;
+    case 2:
+    cout<<"Monday";
+        break;
+    case 3:
+    cout<<"Tuesday";
+        break;
+    case 4:
+    cout<<"Wednesday";
+        break;
+    case 5:
+    cout<<"Thursday";
+        break;
+    case 6:
+    cout<<"Friday";
+        break;
+    case 7:
+    cout<<"Saturday";
+        break;
+    
+    default:
+         cout<<"Invaid Input";
+        break;
+    }
+    return 0;
+}
